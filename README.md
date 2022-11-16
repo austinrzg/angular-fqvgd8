5aa3206aa56338940b4b873ce9e2b89e4bba2a42
austinrzg <118392404+austinrzg@users.noreply.github.com>
2022-11-16 13:43:21 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-fqvgd8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-5xw6cq)
---
+++ src/app/example.component.ts
@@ -0,0 +1,101 @@
+import { Component } from '@angular/core';
+import { PickerController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-example',
+  templateUrl: 'example.component.html',
+})
+export class ExampleComponent {
+  constructor(private pickerCtrl: PickerController) {}
+
+  async openPicker() {
+    const picker = await this.pickerCtrl.create({
+      columns: [
+        {
+          name: 'meat',
+          options: [
+            {
+              text: 'Pepperoni',
+              value: 'pepperoni',
+            },
+            {
+              text: 'Smoked Ham',
+              value: 'smoked-ham',
+            },
+            {
+              text: 'Crispy Bacon',
+              value: 'bacon',
+              disabled: true
+            },
+            {
+              text: 'Turkey',
+              value: 'turkey',
+              disabled: true
+            },
+            {
+              text: 'Venison',
+              value: 'venison',
+              disabled: true
+            },
+            {
+              text: 'Chicken',
+              value: 'chicken'
+            },
+            {
+              text: 'Tofu',
+              value: 'tofu'
+            }
+          ],
+        },
+        {
+          name: 'veggies',
+          options: [
+            {
+              text: 'Red onion',
+              value: 'red-onion',
+            },
+            {
+              text: 'Peppers',
+              value: 'peppers',
+            },
+            {
+              text: 'Black olives',
+              value: 'black-olives',
+            },
+          ],
+        },
+        {
+          name: 'crust',
+          options: [
+            {
+              text: 'Pan style',
+              value: 'pan',
+            },
+            {
+              text: 'Hand tossed',
+              value: 'hand-tossed',
+            },
+            {
+              text: 'Stuffed crust',
+              value: 'stuffed-crust',
+            },
+          ],
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Confirm',
+          handler: (value) => {
+            window.alert(`You selected a ${value.crust.text} pizza with ${value.meat.text} and ${value.veggies.text}`);
+          },
+        },
+      ],
+    });
+
+    await picker.present();
+  }
+}
